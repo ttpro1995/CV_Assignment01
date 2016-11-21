70084ead2e8d71f40915dfff2d5d86471e08197e
thien.thai <tthien@apcs.vn>
2016-11-21 09:18:24 +0700

    add some test

--- test.py
+++ test.py
@@ -7,10 +7,27 @@ import util
 class TestStringMethods(unittest.TestCase):
 
     def test_bgr2gray(self):
-        global cur_img
-        global cat
         cat = cv2.imread('cat.jpg')
         cur_img = util.bgr2gray(cat)
+        self.assertEquals(cur_img.dtype,'uint8')
+
+    def test_derivative(self):
+        cat = cv2.imread('cat.jpg')
+        cat = util.bgr2gray(cat)
+        cur_img_x = util.derivative(cat, mode='x')
+        cur_img_y = util.derivative(cat, mode='y')
+        self.assertEquals(cur_img_x.dtype, 'uint8')
+        self.assertEquals(cur_img_y.dtype, 'uint8')
+
+    def test_smooth(self):
+        cat = cv2.imread('cat.jpg')
+        cat = util.bgr2gray(cat)
+        cur_img_1 = util.smooth(30, 3, cat)
+        cur_img_2 = util.smooth(60, 5, cat)
+        cur_img_3 = util.smooth(0, 3, cat)
+        self.assertEquals(cur_img_1.dtype, 'uint8')
+        self.assertEquals(cur_img_2.dtype, 'uint8')
+        self.assertEquals(cur_img_3.dtype, 'uint8')
 
 if __name__ == '__main__':
     unittest.main()
