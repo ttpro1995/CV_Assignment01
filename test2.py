6aab967f03a7e28fcb595760bed7264b73dade87
thien.thai <tthien@apcs.vn>
2016-11-21 14:56:10 +0700

    pytest

--- test2.py
+++ test2.py
@@ -1,15 +1,15 @@
 # Thai Thien
 # 1351040
 
-import unittest
+import pytest
 import cv2
 import util
-class TestStringMethods(unittest.TestCase):
+class TestUtil():
 
     def test_bgr2gray(self):
         cat = cv2.imread('cat.jpg')
         cur_img = util.bgr2gray(cat)
-        self.assertEquals(cur_img.dtype,'uint8')
+        assert cur_img.dtype =='uint8'
 
     def test_derivative(self):
         cat = cv2.imread('cat.jpg')
@@ -23,9 +23,9 @@ class TestStringMethods(unittest.TestCase):
         cur_img_1 = util.smooth(30, 3, cat)
         cur_img_2 = util.smooth(60, 5, cat)
         cur_img_3 = util.smooth(0, 3, cat)
-        self.assertEquals(cur_img_1.dtype, 'uint8')
-        self.assertEquals(cur_img_2.dtype, 'uint8')
-        self.assertEquals(cur_img_3.dtype, 'uint8')
+        assert cur_img_1.dtype == 'uint8'
+        assert cur_img_2.dtype == 'uint8'
+        assert cur_img_3.dtype == 'uint8'
 
     def test_rotate(self):
         cat = cv2.imread('cat.jpg')
@@ -33,5 +33,3 @@ class TestStringMethods(unittest.TestCase):
         for i in range(0,360):
             ret = util.rotation(cat,i)
 
-if __name__ == '__main__':
-    unittest.main()
